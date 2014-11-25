d6ece2a87bb83733d5d62797edcf612fad7fd629
Androyen <androyen@aol.com>
2014-11-24 19:05:37 -0600

    Format strings and set up view widgets

--- app/src/main/java/com/androyen/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/androyen/interactivestory/ui/StoryActivity.java
@@ -2,17 +2,29 @@ package com.androyen.interactivestory.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.androyen.interactivestory.R;
 import com.androyen.interactivestory.model.Page;
+import com.androyen.interactivestory.model.Story;
 
 public class StoryActivity extends Activity {
 
 
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private String mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +33,45 @@ public class StoryActivity extends Activity {
 
         //Get intent data from MainActivity
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        mName = intent.getStringExtra(getString(R.string.key_name));
 
-        if (name == null) {
-            name = "friend";
+        if (mName == null) {
+            mName = "friend";
         }
 
-       Log.v(TAG, "value of intent extra is " + name);
+       Log.v(TAG, "value of intent extra is " + mName);
+
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceButton1);
+        mChoice2 = (Button)findViewById(R.id.choiceButton2);
+
+        loadPage();
 
 
     }
+
+
+    private void loadPage() {
+
+        //get page from story
+        Page page = mStory.getPage(0);
+
+        Drawable drawable = getResources().getDrawable(page.getImageId());
+        mImageView.setImageDrawable(drawable);
+
+        String pageText = page.getText();
+
+        //Add the name of placeholder included.
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
+    }
+
+
+
 
 
 
